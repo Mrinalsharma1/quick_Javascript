44deb3c05d811781020f079327ef0d2dae0f04e7
Mrinal Sharma <59733255+Mrinalsharma1@users.noreply.github.com>
2023-08-21 18:35:27 +0000

    datatype and typeconversion

---
+++ javascript_log/02_typeConversion.js
@@ -0,0 +1,34 @@
+let score=33
+
+// console.log(score);
+// console.log(typeof score);
+
+let name="hnm"
+// console.log(Number(name)); //NaN
+// console.log(typeof name); //string
+
+// console.log(Boolean(name)); //true
+
+let someNumber =33
+// console.log(typeof String(someNumber)); //string
+// console.log(typeof someNumber); //number
+
+
+
+// ********************** Operations ***********************
+
+let value =3
+// console.log(-value); //-3
+// console.log(value);  //3
+
+// console.log("2"+2+5); //225
+
+// console.log(2+"2"+5); //225
+
+// console.log(2+2+"5"); //45
+
+// console.log(value++); //3
+
+// console.log(value); //4
+
+// console.log(++value); //5
