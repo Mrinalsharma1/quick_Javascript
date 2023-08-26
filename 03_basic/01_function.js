89d9fb92b35ca9c7b60843d549bec915bceeef3c
Mrinal Sharma <59733255+Mrinalsharma1@users.noreply.github.com>
2023-08-26 16:58:08 +0000

    functions and types

---
+++ 03_basic/01_function.js
@@ -0,0 +1,13 @@
+function calculateCartPrice(...item){
+    return item
+}
+// console.log(calculateCartPrice(2,3));
+
+function getObject(anyObj) {
+    return anyObj.name
+}
+
+console.log(getObject({
+    name:'sam',
+    price:999
+}));
